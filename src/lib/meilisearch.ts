8c2cce54dd15ae8a503b26ff7a153358a490ed7b
Rohityd25 <Rohit.ydv.dev@gmail.com>
2026-07-24 12:44:22 +0530

    Fix Meilisearch export type in lib/meilisearch.ts

--- src/lib/meilisearch.ts
+++ src/lib/meilisearch.ts
@@ -1,4 +1,4 @@
-import { MeiliSearch } from 'meilisearch'
+import { Meilisearch as MeiliSearch } from 'meilisearch'
 
 export const meilisearch = new MeiliSearch({
   host: process.env.MEILISEARCH_HOST || 'http://localhost:7700',
